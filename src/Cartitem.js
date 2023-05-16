74a551aaa314d45d5554a40ae8fb5f47bc5f293e
Kelangi Raj Kiran <kelangi.kiran@AD.MSYSTECHNOLOGIES.COM>
2023-05-16 16:35:14 +0530

    added cart item component with css

---
+++ src/Cartitem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Cartitem extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Cart</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Cartitem

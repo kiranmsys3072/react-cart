95595abbec774552ba49173f606773691de612fe
Kelangi Raj Kiran <kelangi.kiran@AD.MSYSTECHNOLOGIES.COM>
2023-05-16 16:35:50 +0530

    added cart item component with css

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -3,10 +3,32 @@ import React from "react";
 class Cartitem extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Cart</h1>
+            <div className="cart-item">
+                <div className="left-block">
+     <img  style={styles.image}/>
+                </div>
+                <div  className="right-block">
+                    <div style={{color:'red'}}>Phone</div>
+                    <div style={{color:'#777'}}>5999</div>
+                    <div style={{color:'#777'}}>qty: 1</div>
+
+                </div>
+                <div className="action-icons">
+                    { /* buttons*/}
+
+                </div>
+                
             </div>
         )
+    }
+}
+
+const styles={
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        backgroundColor:'#777'
     }
 }
 
